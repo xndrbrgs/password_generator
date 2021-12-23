b6e4e88327825dbffe0d3d76e030ab99a1fd149b
xndrbrgs <aborgescolon@gmail.com>
2021-12-23 17:44:43 -0500

    changed initial arrays to ease comma issue

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Arrays for variables needed 
 
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialChar = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
+var numbers = ["0123456789"];
+var upperCaseChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCaseChar = ["abcdefghijklmnopqrstuvwxyz"];
+var specialChar = ["@%+/'!#$^?:,)(}{][~-_."];
 
 var lowerCaseSelected = false;
 var upperCaseSelected = false;
